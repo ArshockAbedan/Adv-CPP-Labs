e77798e84d2f1a643b033c7e34f9e5670a42a065
Aref Abedjooy <arefabedjooy@gmail.com>
2022-02-25 23:51:33 -0500

    Maximum Len of a Concat Str (Unique Characters)

--- MaximumLengthOfAConcatenatedStringWithUniqueCharacters.cpp
+++ MaximumLengthOfAConcatenatedStringWithUniqueCharacters.cpp
@@ -3,8 +3,8 @@
 // Author      : Aref Abedjooy
 // Version     : 1.0
 // Copyright   : Copyright (c) 2022, Aref Abedjooy. No Rights Reserved.
-// Description : You are given an array of strings arr. 
-//               A string s is formed by the concatenation of a subsequence of arr 
+// Description : You are given an array of strings arr.
+//               A string s is formed by the concatenation of a subsequence of arr
 //               that has unique characters.
 //               Return the maximum possible length of s.
 //============================================================================
@@ -13,37 +13,37 @@
 #include <string>
 #include <bitset>
 using namespace std;
- 
-int maxLength(vector<string>& arr)
+
+int maxLength(vector<string> &arr)
 {
-    vector<bitset<26>> dp = { bitset<26>() }; // auxillary dp storage
-    int res = 0; // will store number of unique chars in
-                 // resultant string
-    for (auto& word : arr) {
+    vector<bitset<26>> dp = {bitset<26>()}; // auxillary dp storage
+    int res = 0;                            // will store number of unique chars in
+                                            // resultant string
+    for (auto &word : arr)
+    {
         bitset<26> UniqueCharsofWord; // used to track unique chars
         for (char c : word)
             UniqueCharsofWord.set(c - 'a');
-        int n = UniqueCharsofWord.count();
+        int n = UniqueCharsofWord.count(); // length of unique chars
         if (n < word.size())
             continue; // duplicate chars in current string
- 
-        for (int i = dp.size() - 1; i >= 0; --i) {
+
+        for (int i = dp.size() - 1; i >= 0; i--)
+        {
             bitset<26> current = dp[i];
             if ((current & UniqueCharsofWord).any())
-                continue; // if 1 or more char common
+                continue;                              // if 1 or more char common
             dp.push_back(current | UniqueCharsofWord); // valid concatenation
             res = max(res, (int)current.count() + n);
         }
     }
     return res;
 }
- 
+
 int main()
 {
-    vector<string> v = { "aa", "bb"};
+    vector<string> v = {"cha","r","act","ers"};
     int ans = maxLength(v);
     cout << ans; // resultant answer string : cfbdghzest
     return 0;
 }
-
-
