6c08b31b0b7ff61d2dd42bc76570216e361ec48d
Aref Abedjooy <arefabedjooy@gmail.com>
2022-02-28 23:36:02 -0500

    Construct Quad Tree

---
+++ ConstructQuadTree.cpp
@@ -0,0 +1,67 @@
+//============================================================================
+// Name        : ConstructQuadTree.cpp
+// Author      : Aref Abedjooy
+// Version     : 1.0
+// Copyright   : Copyright (c) 2022, Aref Abedjooy. No Rights Reserved.
+// Description : Given a n * n matrix grid of 0's and 1's only. 
+//               We want to represent the grid with a Quad-Tree.
+//============================================================================
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+using namespace std;
+
+// Definition for a QuadTree node.
+class Node {
+public:
+    bool val;
+    bool isLeaf;
+    Node* topLeft;
+    Node* topRight;
+    Node* bottomLeft;
+    Node* bottomRight;
+    
+    Node() {
+        val = false;
+        isLeaf = false;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+    
+    Node(bool _val, bool _isLeaf) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+    
+    Node(bool _val, bool _isLeaf, Node* _topLeft, Node* _topRight, Node* _bottomLeft, Node* _bottomRight) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = _topLeft;
+        topRight = _topRight;
+        bottomLeft = _bottomLeft;
+        bottomRight = _bottomRight;
+    }
+};
+
+
+class Solution {
+public:
+    Node* construct(vector<vector<int>>& grid) {
+        
+    }
+};
+
+
+int main(){
+    Solution *solution = new Solution();
+    vector<vector<int>> grid;
+    Node *node = solution->construct(grid); 
+    return 0;
+}
