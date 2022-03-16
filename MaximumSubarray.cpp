e1c6334df9977876e0482a98041b9734b232bac8
Aref Abedjooy <arefabedjooy@gmail.com>
2022-03-16 19:18:03 -0400

    MaximumSubarray

--- MaximumSubarray.cpp
+++ MaximumSubarray.cpp
@@ -3,7 +3,7 @@
 // Author      : Aref Abedjooy
 // Version     : 1.0
 // Copyright   : Copyright (c) 2022, Aref Abedjooy. No Rights Reserved.
-// Description : Given an integer array nums, find the contiguous subarray 
+// Description : Given an integer array nums, find the contiguous subarray
 //               (containing at least one number) which has the largest sum and return its sum.
 //============================================================================
 
@@ -12,14 +12,17 @@
 #include <algorithm>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int maxSubArray(vector<int>& nums) {
+    int maxSubArray(vector<int> &nums)
+    {
         int maxGlobal = nums[0];
         int maxCurrent = nums[0];
-        for(int i=1; i < nums.size(); i++){
-            maxCurrent = max(nums[i], maxCurrent+nums[i]);
-            maxGlobal = max(maxGlobal,maxCurrent);
+        for (int i = 1; i < nums.size(); i++)
+        {
+            maxCurrent = max(nums[i], maxCurrent + nums[i]);
+            maxGlobal = max(maxGlobal, maxCurrent);
         }
         return maxGlobal;
     }
@@ -27,7 +30,7 @@ public:
 
 int main()
 {
-    vector<int> arr = {-2,1,-3,4,-1,2,1,-5,4};
+    vector<int> arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     Solution *solution = new Solution();
     cout << solution->maxSubArray(arr);
 }
