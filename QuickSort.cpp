47443ce904ec5e73e2e88179d9688525593c5c8d
Aref Abedjooy <arefabedjooy@gmail.com>
2022-05-16 23:50:09 -0400

    QuickSort

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -8,19 +8,36 @@
 #include <iostream>
 using namespace std;
 
-
-int partition(int arr[], int low, int high){
-    
+int partition(int arr[], int low, int high)
+{
 }
 
+
+/* The main function to implement QuickSort
+arr[] --> Array to be sorted
+low --> starting index
+high --> end index */
 void quickSort(int arr[], int low, int high)
 {
     if (low < high)
     {
+        // pi is partitioning index, arr[pi] is now at right place
         int pi = partition(arr, low, high);
+
+        // seperately sort items befor partition and after partition
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high)
     }
+}
+
+// function to print array
+void printArray(int arr[], int n)
+{
+    for (int i = 0; int i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
 
 int main()
@@ -29,6 +46,6 @@ int main()
     int n = sizeof(arr) / sizeof(arr[0]);
     quickSort(arr, 0, n - 1);
     cout << "Sorted array:" << endl;
-    printArray(arr);
+    printArray(arr, n);
     return 0;
 }
